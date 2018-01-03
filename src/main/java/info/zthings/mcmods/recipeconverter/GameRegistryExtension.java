10b37ca300d73c385e799785b9d04302a6259918
HoldYourWaffle <ravivanrooijen@live.nl>
2018-01-03 20:34:21 +0100

    Sensible parameter names

--- src/main/java/info/zthings/mcmods/recipeconverter/GameRegistryExtension.java
+++ src/main/java/info/zthings/mcmods/recipeconverter/GameRegistryExtension.java
@@ -23,29 +23,29 @@ public abstract class GameRegistryExtension {
 		BLOCK, ITEM;
 	}
 	
-	public static void addRecipe(IRecipe p1) {
+	public static void addRecipe(IRecipe recipe) {
 		//STUB
-		System.out.println("Adding general recipe: " + p1.getIngredients() + " -> " + p1.getRecipeOutput().getCount() + "x" + p1.getRecipeOutput().getItem().getUnlocalizedName());
+		System.out.println("Adding general recipe: " + recipe.getIngredients() + " -> " + recipe.getRecipeOutput().getCount() + "x" + recipe.getRecipeOutput().getItem().getUnlocalizedName());
 	}
 	
-	public static IRecipe addShapedRecipe(ItemStack p0, Object... p1) {
+	public static IRecipe addShapedRecipe(ItemStack output, Object ... params) {
 		//STUB
-		System.out.println("Adding shaped recipe: " + Arrays.toString(p1) + " -> " + p0.getCount() + "x" + p0.getItem().getUnlocalizedName());
+		System.out.println("Adding shaped recipe: " + Arrays.toString(params) + " -> " + output.getCount() + "x" + output.getItem().getUnlocalizedName());
 		
-		ShapedPrimer primer = CraftingHelper.parseShaped(p1);
+		ShapedPrimer primer = CraftingHelper.parseShaped(params);
 		String group = "test"; //STUB
-        return new ShapedRecipes(group == null ? "" : group.toString(), primer.width, primer.height, primer.input, p0);
+        return new ShapedRecipes(group == null ? "" : group.toString(), primer.width, primer.height, primer.input, output);
 	}
-	public static void addRecipe(ItemStack p0, Object... p1) { addShapedRecipe(p0, p1); }
+	public static void addRecipe(ItemStack output, Object ... params) { addShapedRecipe(output, params); }
 	
-	public static void addShapelessRecipe(ItemStack p0, Object... p1) {
+	public static void addShapelessRecipe(ItemStack output, Object ... params) {
 		//STUB
-		System.out.println("Adding shapeless recipe: " + Arrays.toString(p1) + " -> " + p0.getCount() + "x" + p0.getItem().getUnlocalizedName());
+		System.out.println("Adding shapeless recipe: " + Arrays.toString(params) + " -> " + output.getCount() + "x" + output.getItem().getUnlocalizedName());
 	}
 	
-	public static <K extends IForgeRegistryEntry<IRecipe>> K register(K p0, ResourceLocation p1) {
+	public static <K extends IForgeRegistryEntry<IRecipe>> K register(K recipe, ResourceLocation location) {
 		//STUB
-		System.out.println("Adding a '" + p0.getRegistryType().getSimpleName().toLowerCase().replace("recipe", "") + "' recipe: " + p1.toString());
-		return p0;
+		System.out.println("Adding a '" + recipe.getRegistryType().getSimpleName().toLowerCase().replace("recipe", "") + "' recipe: " + location.toString());
+		return recipe;
 	}
 }
